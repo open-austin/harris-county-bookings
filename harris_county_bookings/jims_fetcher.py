349f451d1fb0bf12badd1b4a00cfa9c68f9883c9
Colby M. White <colby.white@utexas.edu>
2016-08-13 16:23:56 -0500

    scrub personal data from JIMS report
    Closes #4
    
    this uses the CSV library to filter out certain headers. as a result
    the data is saved as CSV instead of ACCDB. Will add a dialect in order
    to save as either file type

--- harris_county_bookings/jims_fetcher.py
+++ harris_county_bookings/jims_fetcher.py
@@ -1,3 +1,5 @@
+import csv
+import io
 import os
 
 import requests
@@ -7,9 +9,26 @@ from github_publisher import GitHubPublisher
 
 
 class JIMSFetcher(object):
+    # The headers left out of this list are the headers that contain personal data
+    # https://github.com/open-austin/harris-county-bookings/issues/4
+    HEADERS_TO_SAVE = ['SEX', 'RACE', 'ARREST DATE', 'BOOKING DATE', 'ADDRESS NUMBER', 'ADDRESS PREFIX',
+                       'ADDRESS STREET', 'ADDRESS SUFFIX', 'ADDRESS ALI', 'ADDRESS CITY', 'ADDRESS STATE',
+                       'ADDRESS ZIP', 'CHARGE CODE', 'CHARGE WORDING', 'CHARGE LEVEL', 'DISPOSITION']
+
     @staticmethod
     def read():
         return requests.get(JIMS_1058_URL).text
+
+    @staticmethod
+    def parse():
+        raw_data = JIMSFetcher.read().split('\n')
+        headers = raw_data[0].split(';')
+        data = list(csv.DictReader(raw_data[1:], fieldnames=headers, delimiter=';'))
+        return map(JIMSFetcher.strip_whitespace_from_values, data)
+
+    @staticmethod
+    def strip_whitespace_from_values(dictionary):
+        return {k: v.strip() for k, v in dictionary.iteritems()}
 
     @staticmethod
     def save_to_file(date, directory=DATA_DIR):
@@ -17,20 +36,32 @@ class JIMSFetcher(object):
         dirname = os.path.dirname(file_path)
         if not os.path.exists(dirname):
             os.makedirs(dirname)
+        data = JIMSFetcher.parse()
         with open(file_path, 'w+') as f:
-            f.write(JIMSFetcher.read())
+            JIMSFetcher.write_csv(f, data)
         return file_path
 
     @staticmethod
+    def write_csv(output, data):
+        # specifying ignore will skip the entries not in HEADERS_TO_SAVE
+        writer = csv.DictWriter(output, JIMSFetcher.HEADERS_TO_SAVE, extrasaction='ignore')
+        writer.writeheader()
+        writer.writerows(data)
+        return output
+
+    @staticmethod
     def save_to_github_commit(date):
-        file_path = JIMSFetcher.build_file_path(date, DATA_DIR)
         # Do the import here so the rest of the methods work without defining settings
         import settings
+
+        file_path = JIMSFetcher.build_file_path(date, DATA_DIR)
+        data = JIMSFetcher.parse()
+        output = JIMSFetcher.write_csv(io.BytesIO(), data)
         publisher = GitHubPublisher(settings.GITHUB_REPO_NAME, settings.GITHUB_API_TOKEN)
         # TODO verify the token has commit access to the repo
-        return publisher.publish(file_path, settings.GITHUB_BRANCH, JIMSFetcher.read())
+        return publisher.publish(file_path, settings.GITHUB_BRANCH, output.getvalue())
 
     @staticmethod
     def build_file_path(date, directory):
-        fileName = "%s.accdb" % date.strftime('%Y-%m-%d')
+        fileName = "%s.csv" % date.strftime('%Y-%m-%d')
         return os.path.join(directory, str(date.year), fileName)

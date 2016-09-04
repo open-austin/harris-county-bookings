cf31ae9fba85e51cd854c2bce44e229580ac0ca0
Colby M. White <colbywhite@users.noreply.github.com>
2016-09-04 12:29:48 -0600

    adding raw/scrub modes
    this is to allow for commits to a separate repo to hold raw data.
    
    closes #9

--- harris_county_bookings/settings_example.py
+++ harris_county_bookings/settings_example.py
@@ -1,5 +1,7 @@
 # These settings are only needed if you are planning to push to GitHub.
 # If you only are doing saves to a local file, then these are not needed.
 GITHUB_API_TOKEN = 'YOUR_GITHUB_TOKEN_HERE'
-GITHUB_BRANCH = 'THE_BRANCH_YOU_WANT_TO_PUSH_TO'
-GITHUB_REPO_NAME = 'THE_REPO_YOU_WANT_TO_PUSH_TO' # i.e openaustin/harris-county-bookings
+GITHUB_REPOS = {
+    'scrub': {'name': 'THE_PUBLIC_REPO_YOU_WANT_TO_PUSH_TO', 'branch': 'THE_BRANCH_YOU_WANT_TO_PUSH_TO'},
+    'raw': {'name': 'THE_PRIVATE_REPO_YOU_WANT_TO_PUSH_TO', 'branch': 'THE_BRANCH_YOU_WANT_TO_PUSH_TO'}
+}
--- save_today.py
+++ save_today.py
@@ -4,30 +4,37 @@ import argparse
 from datetime import date
 
 from harris_county_bookings import *
+from harris_county_bookings.constants import *
 
 
 # noinspection PyUnusedLocal
 def lambda_handler(event, context):
-    save_today_as_commit()
+    save_today_as_commit(ALL_MODES)
 
 
-def save_today_as_commit():
-    result = JIMSRecorder.save_to_github_commit(date.today())
+def save_today_as_commit(modes):
+    result = JIMSRecorder.save_to_github_commit(date.today(), modes)
     if result:
         urls = {r['content']['html_url'] for r in result}
         print ' '.join(urls)
 
 
-def save_today_as_file():
-    print ' '.join(JIMSRecorder.save_to_file(date.today()))
+def save_today_as_file(modes):
+    print ' '.join(JIMSRecorder.save_to_file(date.today(), modes))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Save today\'s JIMS 1058 report')
     parser.add_argument('--commit', action='store_true',
                         help='If true, the file will be saved as a commit. Otherwise it is saved as a local file.')
+    data_mode_help = 'Define which mode to scrub the data. \'%s\' will save the data as is. ' \
+                     '\'%s\' will scrub out personal data. \'both\' will save files for ' \
+                     'each mode in separate directories.' % (RAW, SCRUB)
+    parser.add_argument('--data_mode', help=data_mode_help, default=SCRUB, choices=[RAW, SCRUB, 'both'])
     args = parser.parse_args()
+    data_modes = [args.data_mode] if args.data_mode != 'both' else ALL_MODES
+
     if args.commit:
-        save_today_as_commit()
+        save_today_as_commit(data_modes)
     else:
-        save_today_as_file()
+        save_today_as_file(data_modes)
